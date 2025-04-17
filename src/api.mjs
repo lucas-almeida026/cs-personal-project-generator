2a0670c894094f9ebd4835b5111e3da762e35154
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2025-04-17 13:58:12 -0300

    1. create project, 2. refine epic names, 3. create tasks for epic

--- src/api.mjs
+++ src/api.mjs
@@ -9,59 +9,53 @@ const ollamaClient = axios.create({
 })
 
 export const ollamaApi = {
-	system: `Você é um assistente de IA ajudando estudantes de ciência da computação a planejar e executar um projeto pessoal significativo, focado exclusivamente no desenvolvimento de software.
+	projectSuggestor: {
+		_system: `Você é um assistente de IA que ajuda estudantes de ciência da computação a planejar projetos pessoais de software usando uma estrutura inspirada em ferramentas de gerenciamento de tarefas como o JIRA.
 
 Sua missão é, com base nas respostas de um formulário:
 
 1. Entender a área de interesse do estudante (web, mobile ou sistemas);
 2. Identificar interesses e frustrações pessoais do dia a dia;
-3. Sugerir um projeto original e motivador, combinando interesses e problemas reais;
-4. Criar um backlog com tarefas de programação claras, pequenas e acionáveis, que foquem **exclusivamente no desenvolvimento do projeto**. Evite qualquer tarefa genérica, vaga ou pós-desenvolvimento como "testar", "refatorar", "publicar" ou "documentar";
-5. Para cada tarefa, adicione de 0 a 3 tópicos relacionados úteis para aprofundamento técnico;
-6. Liste também as páginas (web), telas (mobile) ou scripts/algoritmos (sistemas) necessários para o projeto, dependendo da área escolhida.
+3. Sugerir um projeto original, motivador e tecnicamente viável;
+4. Gerar uma lista de épicos, ou seja, os grandes módulos funcionais do projeto, como “Autenticação”, “Catálogo de Produtos” ou “Controle de Usuários”.
+
+Cada épico deve representar uma funcionalidade clara da aplicação, e servirá como base para dividir as tarefas futuras.
 
 ### Responda estritamente no seguinte formato, sem nenhum texto fora da estrutura:
-
+	
 [item-start](project)  
 	[string](name) <nome curto do projeto>  
 	[string](description) <parágrafo curto explicando por que este projeto é adequado>  
-	[list-start](tasks)  
-		[item-start](task1)  
-			[string](name) <título curto da tarefa - o que precisa ser feito>  
-			[string](description) <descrição detalhada e específica da tarefa>  
-			[list-start](related_topics)  
-				[string] <tópico 1>  
-				[string] <tópico 2>  
-				[string] <tópico 3>  
-			[list-end]  
-		[item-end]  
-		... (repita para cada tarefa)  
-	[list-end]  
+	[list-start](epics)  
+		[string] <nome>
+		[string] <nome>
+		[string] <nome>
+		...(outros épicos)
+	[list-end]
 [item-end]
 
-**Exemplo válido**:
+**Exemplo correto :)**:
 [item-start](project)  
-	[string](name) GymChat  
-	[string](description) Aplicativo de chat entre amigos que treinam em academias diferentes, permitindo comunicação em tempo real e envio de mensagens assíncronas.  
-	[list-start](tasks)  
-		[item-start](task1)  
-			[string](name) Criar tela de login com autenticação  
-			[string](description) Usar Firebase Authentication para criar uma tela de login funcional, com validação de e-mail e senha.  
-			[list-start](related_topics)  
-				[string] Firebase Authentication  
-				[string] Validação de formulários  
-				[string] JSON Web Tokens  
-			[list-end]  
-		[item-end]  
-		...(Mais 8 outras tarefas) 
-	[list-end]  
+	[string](name) Finn App  
+	[string](description) Aplicativo de gerenciamento de finanças pessoais
+	[list-start](epics)  
+		[string] Autenticação
+		[string] Transações
+		[string] Contas
+		[string] Contas poupança
+		[string] Orçamentos
+		[string] Categorias
+		[string] Notificações
+		[string] Relatórios
+	[list-end]
 [item-end]
 
 ### Diretrizes obrigatórias:
 
-- Gere **ao menos 9 tarefas**, todas granulares e voltadas à construção do projeto.
-- Não inclua tarefas genéricas como "teste", "debug", "refatoração", "melhorias", "deploy", "documentação", etc.
-- Foque em ações pequenas, claras e incrementais, como "Criar botão de login", "Salvar dados em banco local", "Validar campo de email".
+- Gere **ao menos 5 épicos**, e idealmente entre 6 e 8.
+- Cada épico deve representar um módulo funcional claro e específico, como "Autenticação", "Gerenciamento de produtos", "Personalização", etc.
+- Cada épico de ser uma única palavra ou no máximo duas palavras.
+- Não escreva frases curtas para nomear épicos como "Janela de Aprendizado de Inglês", "Integração com Bibliotecas de Música" ou "Gerenciamento de ordens recebidas"
 - Nunca escreva nenhum texto fora da estrutura especificada.
 - Sempre responda em **português brasileiro**.
 - O nome do projeto deve ser curto, único e memorável.
@@ -71,53 +65,187 @@ Sua missão é, com base nas respostas de um formulário:
 - Nunca escreva sobre você, seu funcionamento, ou explique o que está fazendo.
 - Escolha um projeto que permita aplicar conhecimentos típicos do 1º ou 2º ano da faculdade de ciência da computação.
 `,
-	async generateSuggestion({ area, topics, problems, dreams, time }) {
-		const prompt = `Sou um estudante de ciência da computação, ${this._incorporateArea(area)}. Me interesso naturalmente por ${topics}. No dia a dia enfrento os seguintes problemas ${problems}, seria legal resolver algum deles com uma solução que eu mesmo fiz. Se pudesse fazer algo do zero, gostaria de ${dreams}. Tenho ${this._incorporateTime(time)} horas por semana para me dedicar ao projeto pessoal.`
-		const body = {
-			model: "llama3.2:3b",
-			system: this.system,
-			stream: false,
-			temperature: 0.3,
-			prompt: prompt,
-		}
-		console.log(body)
-		try {
-			const response = await ollamaClient.post('/?path=/api/generate', body)
-			const data = response.data
-			let modelResponse = data?.response
-			if (!modelResponse || typeof modelResponse !== 'string') {
-				throw new Error('No response from model')
+		async generate({ area, topics, problems, dreams, time }) {
+			const prompt = `Sou um estudante de ciência da computação, ${this._incorporateArea(area)}. Me interesso naturalmente por ${topics}. No dia a dia enfrento os seguintes problemas ${problems}, seria legal resolver algum deles com uma solução que eu mesmo fiz. Se pudesse fazer algo do zero, gostaria de ${dreams}. Tenho ${this._incorporateTime(time)} horas por semana para me dedicar ao projeto pessoal.`
+			const body = {
+				model: "llama3.1:8b",
+				system: this._system,
+				stream: false,
+				temperature: 0.5,
+				prompt: prompt,
 			}
-			return modelResponse
-		} catch(error) {
-			console.error('Error generating suggestion:', error)
-			throw new Error('Failed to generate suggestion')
+			try {
+				const response = await ollamaClient.post('/?path=/api/generate&tag=project_suggestor', body)
+				const data = response.data
+				let modelResponse = data?.response
+				if (!modelResponse || typeof modelResponse !== 'string') {
+					throw new Error('No response from model')
+				}
+				return modelResponse
+			} catch (error) {
+				console.error('Error generating suggestion:', error)
+				throw new Error('Failed to generate suggestion')
+			}
+		},
+		_incorporateArea(area) {
+			switch (area) {
+				case 'web':
+					return 'quero aprender sobre desenvolvimento web'
+				case 'mobile':
+					return 'quero aprender sobre desenvolvimento mobile'
+				case 'systems':
+					return 'quero aprender sobre desenvolvimento de sistemas desktop, scripts de automação ou até ferramentas de linha de comando'
+				default:
+					return 'ainda não decidi o que quero aprender'
+			}
+		},
+		_incorporateTime(time) {
+			switch (time) {
+				case 'lt_2':
+					return 'menos de duas'
+				case 'bt_2-4':
+					return 'entre duas e quatro'
+				case 'bt_5-7':
+					return 'entre cinco e sete'
+				case 'mt_8':
+					return 'mais de oito'
+				default:
+					return 'poucas'
+			}
 		}
 	},
-	_incorporateArea(area) {
-		switch (area) {
-			case 'web':
-				return 'quero aprender sobre desenvolvimento web'
-			case 'mobile':
-				return 'quero aprender sobre desenvolvimento mobile'
-			case 'systems':
-				return 'quero aprender sobre desenvolvimento de sistemas desktop, scripts de automação ou até ferramentas de linha de comando'
-			default:
-				return 'ainda não decidi o que quero aprender'
+	epicShrinker: {
+		_system: `Você é um assistente de IA que ajdua a resumir frases de tamanho médio.
+Por ser um especialista em capturar a essência de uma frase em um única palavra (no máximo duas) que carrega também os aspecto técnico, visto que a palavra escolhida será o nome de um mídulo de um software em desenvolvimento, exepmlos do seu trabalho são:
+
+- "Cadastro de usuários" -> "Autenticação"
+- "Busca por letras de canções" -> "Busca"
+- "Áudio para reprodução das letras" -> "Reprodução"
+- "Controle de Progresso" -> "Progresso"
+- "Assinatura e Download de Vídeos" -> "Monetização"
+- "Notificações de Novos Capítulos" -> "Notificações"
+- "Autenticação" -> "Autenticação"
+- "Produtos" -> "Produtos"
+
+Você é tão bom que consegue fazer esse processo para várias frases de uma só vez.
+Responda como você sempre faz, mas não escreva nada fora da estrutura especificada.
+Perceba que quando a frase já está adequada você não precisa fazer nada, e pode simplesmente repetir a frase.
+
+Continue seu bom trabalho e reescreava a frase a seguir: `,
+		async generate(strings) {
+			const prompt = `${strings.map(s => `- "${s}" -> ?`).join('\n')}`
+			const body = {
+				model: "llama3.1:8b",
+				system: this._system,
+				stream: false,
+				temperature: 0.3,
+				prompt: prompt,
+			}
+			try {
+				const response = await ollamaClient.post('/?path=/api/generate&tag=epic_shrinker', body)
+				const data = response.data
+				let modelResponse = data?.response
+				if (!modelResponse || typeof modelResponse !== 'string') {
+					throw new Error('No response from model')
+				}
+				console.log({ modelResponse })
+				return modelResponse
+					.split('\n')
+					.map(line => line.includes('>')
+						? line.split('>')?.[1]?.replaceAll(/[\"\']/g, '')?.trim()
+						: line.replaceAll(/[\"\'-]/g, '')?.trim())
+					.filter(line => line?.length > 0)
+					.map((l, i) => ({
+						original: strings[i],
+						summarized: l,
+					}))
+			} catch (error) {
+				console.error('Error generating suggestion:', error)
+				throw new Error('Failed to generate suggestion')
+			}
 		}
 	},
-	_incorporateTime(time) {
-		switch (time) {
-			case 'lt_2':
-				return 'menos de duas'
-			case 'bt_2-4':
-				return 'entre duas e quatro'
-			case 'bt_5-7':
-				return 'entre cinco e sete'
-			case 'mt_8':
-				return 'mais de oito'
-			default:
-				return 'poucas'
+	taskGenerator: {
+		_system: `Você é uma IA que atua como Product Owner e Tech Lead ao mesmo tempo. Seu papel é quebrar épicos de um projeto de software em tarefas claras, organizadas e viáveis para desenvolvimento, seguindo uma estrutura de planejamento semelhante à usada em times ágeis com JIRA.
+
+Você receberá as seguintes informações:
+- Nome do projeto;
+- Descrição do projeto (seu objetivo geral);
+- Um dos épicos do projeto (módulo funcional de alto nível).
+
+Sua missão é:
+1. Com base no nome, descrição e épicos, entender o escopo da aplicação;
+2. Gerar uma lista de até **10 tarefas** para o épico informado;
+3. As tarefas devem ser específicas, curtas, diretas, e representarem ações de desenvolvimento compreensíveis para devs no início da carreira;
+4. Evitar tarefas genéricas demais como "implementar sistema de contas" — sempre divida em ações claras;
+5. Evitar tarefas excessivamente técnicas como "refatorar handler assíncrono", a não ser que sejam necessárias para o fluxo principal;
+6. Preferir linguagem simples como "criar", "editar", "remover", "exibir", "agendar", "listar", etc.
+
+### Formato de resposta obrigatório:
+
+[item-start](epic)
+	[string](name) <nome do épico>
+	[list-start](tasks)
+		[string] <tarefa 1 relacionada ao épico>
+		[string] <tarefa 2 relacionada ao épico>
+		[string] <tarefa 2 relacionada ao épico>
+		[string] <tarefa 2 relacionada ao épico>
+		[string] <tarefa 2 relacionada ao épico>
+		...(até no máximo 10 tarefas por épico)
+	[list-end]
+[item-end]
+
+### Exemplo de saída válida:
+
+[item-start](epic)  
+	[string](name) Autenticação  
+	[list-start](tasks)  
+		[string] Criar conta com nome e email  
+		[string] Validar email do usuário  
+		[string] Fazer login com nome e email  
+		[string] Implementar logout  
+		[string] Proteger rotas com token de autenticação  
+		[string] Adicionar recuperação de senha  
+		[string] Implementar troca de senha  
+	[list-end]  
+[item-end]
+
+- Cada tarefa deve ser curta, clara e autônoma (ideal para virar um ticket de desenvolvimento).
+- Sempre escreva em **português brasileiro**.
+- Não escreva nenhum texto fora da estrutura especificada.
+- Nunca escreva explicações, comentários ou anotações.
+- Nunca escreva sobre você ou seu funcionamento interno.
+- Não utilize emojis ou caracteres especiais.
+- Não utilize formatação Markdown, como negrito ou itálico.
+- Não utilize aspas ou parênteses.
+- Não utilize listas numeradas ou marcadores.
+- Não utilize caracteres especiais como "@" ou "#".
+- Não utilize abreviações ou siglas.
+`,
+		async generate({ name, description, epic }) {
+			const prompt = `Nome do projeto: ${name}
+Descrição do projeto: ${description}
+Epico: ${epic}
+`;
+			const body = {
+				model: "llama3.1:8b",
+				system: this._system,
+				stream: false,
+				temperature: 0.5,
+				prompt: prompt,
+			}
+			try {
+				const response = await ollamaClient.post('/?path=/api/generate&tag=task_generator', body)
+				const data = response.data
+				let modelResponse = data?.response
+				if (!modelResponse || typeof modelResponse !== 'string') {
+					throw new Error('No response from model')
+				}
+				return modelResponse
+			} catch (error) {
+				console.error('Error generating suggestion:', error)
+				throw new Error('Failed to generate suggestion')
+			}
 		}
 	}
 }
--- src/server.mjs
+++ src/server.mjs
@@ -22,38 +22,69 @@ server.get('/info', (_, res) => {
 	});
 })
 
-server.post('/process', async (req,res) => {
-	const {  area, topics, problems, dreams, time } = req.body
-	const response = await ollamaApi.generateSuggestion({ area, topics, problems, dreams, time })
+server.post('/process', async (req, res) => {
+	console.log('[POST] /process')
+	const { area, topics, problems, dreams, time } = req.body
+	const response = await ollamaApi.projectSuggestor.generate({ area, topics, problems, dreams, time })
 	if (!response || typeof response !== 'string') {
 		return res.status(500).send({
 			error: 'No response from model',
 		})
 	}
-fs.writeFileSync('./response.txt', response, 'utf-8')
-	const r = parseCustomFormat(response)
-	fs.writeFileSync('./parsed.json', JSON.stringify(r, null, 2), 'utf-8')
-	const { name, description, tasks } = r
-	const html = `<div class="container mx-auto p-4">
-	<h1 class="text-2xl font-bold">${name}</h1>
-	<h3 class="text-slate-700">${description}</h3><br/>
-	<h4 class="text-lg font-bold">Tasks:</h4>
-	<div class="grid grid-cols-1 gap-4">
-		${tasks?.map(task => `
-			<div class="border border-gray-300 rounded-md p-4">
-				<h5 class="text-md font-semibold ">${task?.name}</h5>
-				<p class="text-slate-700 text-sm">${task?.description}</p>
-				<ul class="list-disc ml-5">
-					${task?.related_topics?.map(topic => `<li class="text-slate-700">${topic}</li>`).join('')}
-				</ul>
-			</div>`).join('')}
-	</div>
-	</div>`
-	res.send(html)
+	fs.writeFileSync('./project.txt', response, 'utf-8')
+	try {
+		let {name, description, epics} = parseCustomFormat(response)
+		const cleanEpics = await ollamaApi.epicShrinker.generate(epics ?? [])
+		epics = cleanEpics
+		fs.writeFileSync('./project.json', JSON.stringify({name, description, epics}, null, 2), 'utf-8')
+
+		console.log({cleanEpics})
+		const firstepic = epics?.[0]
+		if (!firstepic || !('original' in firstepic) || !('summarized' in firstepic)) {
+			throw new Error('No epics found')
+		}
+		let contextualEpic = firstepic.summarized
+		if (contextualEpic !== firstepic.original) {
+			contextualEpic += ` (${firstepic.original})`
+		}
+		const firstEpicTasks = await ollamaApi.taskGenerator.generate({ name, description, epic: contextualEpic })
+		fs.writeFileSync('./tasks1.txt', firstEpicTasks, 'utf-8')
+		const tasks = parseCustomFormat(firstEpicTasks)
+		fs.writeFileSync('./tasks1.json', JSON.stringify(tasks, null, 2), 'utf-8')
+	} catch (e) {
+		console.error('Error:', e)
+	}
+	// const html = `<div class="container mx-auto p-4">
+	// <h1 class="text-2xl font-bold">${name}</h1>
+	// <h3 class="text-slate-700">${description}</h3><br/>
+	// <h4 class="text-lg font-bold">Tasks:</h4>
+	// <div class="grid grid-cols-1 gap-4">
+	// 	${tasks?.map(task => `
+	// 		<div class="border border-gray-300 rounded-md p-4">
+	// 			<h5 class="text-md font-semibold ">${task?.name}</h5>
+	// 			<p class="text-slate-700 text-sm">${task?.description}</p>
+	// 			<ul class="list-disc ml-5">
+	// 				${task?.related_topics?.map(topic => `<li class="text-slate-700">${topic}</li>`).join('')}
+	// 			</ul>
+	// 		</div>`).join('')}
+	// </div>
+	// </div>`
+	res.send('<p>ok</p>')
 })
 
 server.post('/ollama', async (req, res) => {
 	const path = req.query.path || '/';
+	const tag = req.query.tag || 'unknown';
+	const {system, prompt} = req.body;
+
+	const logEntry = {
+		timestamp: new Date(),
+		tag: tag,
+		request: {
+			system,
+			prompt
+		}
+	}
 	try {
 		const response = await fetch(env.OLLAMA_HOST + path, {
 			method: 'POST',
@@ -64,12 +95,27 @@ server.post('/ollama', async (req, res) => {
 			timeout: 60000,
 		});
 		const data = await response.json();
+		logEntry.response = data?.response;
 		res.send(data);
 	} catch (error) {
 		console.error('Error[OllamaProxy]:', error);
+		logEntry.error = error.message;
 		res.status(500).send({
 			error: 'Failed to connect to OLLAMA API',
 			message: error.message,
+		});
+	} finally {
+		const logFilePath = `./llama.log`;
+		const logEntryString = `time: ${logEntry.timestamp.toISOString()}
+tag: ${logEntry.tag}
+request: ${logEntry.request.prompt.replaceAll(/[\r\n]+/g, '//')}
+response: ${logEntry.response || 'none'}
+error: ${logEntry.error || 'none'}
+--------------------\n`;
+		fs.appendFile(logFilePath, logEntryString, (err) => {
+			if (err) {
+				console.error('Error writing to log file:', err);
+			}
 		});
 	}
 });
